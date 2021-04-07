e061d6d73adda45647e8c6c28d60d085bfaf1993
Hanna Lushchyk <anna.luschik@yandex.ru>
2021-04-07 20:26:01 +0300

    fix: replaced http in source by https

--- src/YouTube/components/Frame/Frame.jsx
+++ src/YouTube/components/Frame/Frame.jsx
@@ -7,7 +7,7 @@ const Frame = React.memo(({ id }) => {
   return (
     <iframe
       title={id}
-      src={`http://www.youtube.com/embed/${id}?enablejsapi=1&start=${searchParams.start}&origin=https://annluschik.github.io/Video-Search-App/`}
+      src={`https://www.youtube.com/embed/${id}?enablejsapi=1&start=${searchParams.start}&origin=https://annluschik.github.io/Video-Search-App/`}
       width="340"
       height="190"
       frameBorder="0"
